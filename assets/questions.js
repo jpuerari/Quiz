abc62393d421c33667eff61882b0b142c7386d9e
Joseph D. Puerari <joep@Joes-MacBook-Pro.local>
2019-12-15 16:09:16 -0500

    laying out variables

---
+++ assets/questions.js
@@ -0,0 +1,26 @@
+alert("Welcome to the Spain geography quiz!");
+
+
+
+var questions = [
+  {
+    question: "What is the capital of Spain?",
+    choices: ["A. Guadalajara.", "B. Valencia.", "C. Barcelona", "D Madrid"],
+    answer: "D. Madrid."
+  },
+  {
+    question: "The Valencian Community is located along the Mediterranean Sea",
+    choices: ["True", "False"],
+    answer: "True"
+  }
+  {
+    question: "Is Spain a Constitutional Monarchy?",
+    choices: ["True", "False"],
+    answer: "True"
+  },
+  {
+    question: "Within the Valencian Community, which language is spoken along with Spanish?",
+    choices: ["A. Galician", "B. Valencian", "C. Andalusian", "D. Basque"],
+    answer: "B. Valencian."
+  },
+];
